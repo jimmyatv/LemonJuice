3ca6c6c789a5530482724b9ebc193ed6626fc030
jimmyatv <rasic.alexandar@gmail.com>
2023-02-27 13:04:15 +0100

    navBar correction

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-const burger = document.querySelector('.burger');
-const mobileNav = document.querySelector('.mobile-nav');
+// const burger = document.querySelector('.burger');
+// const mobileNav = document.querySelector('.mobile-nav');
 
-burger.addEventListener('click', () => {
-    burger.classList.toggle('is-active');
-    mobileNav.classList.toggle('is-active');
-});
+// burger.addEventListener('click', () => {
+//     burger.classList.toggle('is-active');
+//     mobileNav.classList.toggle('is-active');
+// });
